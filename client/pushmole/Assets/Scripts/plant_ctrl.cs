01d0a0a7eec30efba8d3cf85092bb16734aea266
kingstop <kingstop@sina.com>
2016-03-22 18:49:29 +0800

    add some plant

---
+++ client/pushmole/Assets/Scripts/plant_ctrl.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class plant_ctrl : MonoBehaviour {
+    public GameObject[] _plants;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    public void random_plant()
+    {
+        int length = _plants.Length;
+        int plant = Random.Range(0, length);
+        for(int i = 0; i < length; i ++)
+        {
+            if(plant == i)
+            {
+                _plants[i].SetActive(true);
+            }
+            else
+            {
+                _plants[i].SetActive(false);
+            }
+        }
+
+    }
+}
