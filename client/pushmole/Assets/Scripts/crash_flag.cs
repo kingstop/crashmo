5523606e0b1cb92932cb98792e1891bef708a9bf
kingstop <kingstop@sina.com>
2016-04-11 14:48:17 +0800

    a

--- client/pushmole/Assets/Scripts/crash_flag.cs
+++ client/pushmole/Assets/Scripts/crash_flag.cs
@@ -5,16 +5,12 @@ public class crash_flag : MonoBehaviour {
 
 	// Use this for initialization
     public GameObject flag;
-    public GameObject flag_pole;
 
     public void set_position(float x, float y, float z)
     {
         y -= 1.166f;
         Vector3 vc = new Vector3(x, y, z);
         this.transform.position = vc;
-        //flag.transform.position = vc;
-        //flag_pole.transform.position = vc;
-        //  _creature_physics.transform.position = _current_position;
     }
 
     public Vector3 get_position()
