3398378c89680ba223d456e0590094606264a38e
kingstop <kingstop@sina.com>
2016-05-13 15:48:14 +0800

    啊大帝

--- client/pushmole/Assets/Scripts/CrashPlayer.cs
+++ client/pushmole/Assets/Scripts/CrashPlayer.cs
@@ -102,7 +102,6 @@ public class CrashPlayer
                                 list_entry.Value.Remove(key_entry.Key);
                                 break;
                             }
-
                         } 
                     }
                 }
