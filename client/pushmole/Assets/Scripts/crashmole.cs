489ca4d8b3bff907b5e670d73195995210261969
kingstop <apple@iMac-27-ME088.local>
2016-10-25 09:05:15 +0800

    add

--- client/pushmole/Assets/Scripts/crashmole.cs
+++ client/pushmole/Assets/Scripts/crashmole.cs
@@ -823,7 +823,12 @@ public class crash_manager
         move_list(_lock_mole, dir);
     }
     public void catch_click(int i, bool his = false)
-    {
+	{
+		if (global_instance.Instance._crash_manager._record._open_record) 
+		{
+			return;
+		}
+
         if(_creature._is_in_falldown == false)
         {
             if (i == 0)
