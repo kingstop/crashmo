826709d92a4b183448262fc5fcbe2ff5c623c4bc
kingstop <kingstop@sina.com>
2017-03-01 17:40:52 +0800

    add

--- server/server/server/common/utilities.cpp
+++ server/server/server/common/utilities.cpp
@@ -600,25 +600,25 @@ void convert_unix_time( unsigned int t, int* outyear, int* outmonth, int* outday
 
 	time_t _t = t;
 
-	tm* p;
+	tm p;
 
 #ifdef _WIN32
-	p = localtime(&_t);
+	localtime_s(&p,&_t);
 #else
-	localtime_r(&_t, p);
+	localtime_r(&_t, &p);
 #endif
 
-	*outyear = p->tm_year + 1900;
+	*outyear = p.tm_year + 1900;
 
-	*outmonth = p->tm_mon + 1;
+	*outmonth = p.tm_mon + 1;
 
-	*outday = p->tm_mday;
+	*outday = p.tm_mday;
 
-	*outhour = p->tm_hour;
+	*outhour = p.tm_hour;
 
-	*outminute = p->tm_min;
+	*outminute = p.tm_min;
 
-	*outsecond = p->tm_sec;
+	*outsecond = p.tm_sec;
 
 }
 
