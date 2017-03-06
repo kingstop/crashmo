fa9deabe2c0ea2a2aecd642a00d5df4c662f518a
kingstop <kingstop@sina.com>
2017-03-06 18:33:54 +0800

    add

---
+++ client/pushmole/Assets/FPSGUI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSGUI : MonoBehaviour {
+
+    public float updateInterval = 0.5F;
+    private double lastInterval;
+    private int frames = 0;
+    private float fps;
+    void Start()
+    {
+        lastInterval = Time.realtimeSinceStartup;
+        frames = 0;
+    }
+    void OnGUI()
+    {
+        GUILayout.Label(" " + fps.ToString("f2"));
+    }
+    void Update()
+    {
+        ++frames;
+        float timeNow = Time.realtimeSinceStartup;
+        if (timeNow > lastInterval + updateInterval)
+        {
+            fps = (float)(frames / (timeNow - lastInterval));
+            frames = 0;
+            lastInterval = timeNow;
+        }
+    }
+}
