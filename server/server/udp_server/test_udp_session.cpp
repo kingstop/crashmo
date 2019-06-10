8b6c4f08f9d524497b84c4f87fee5d0737fbba49
kingstop <kingstop@sina.com>
2019-06-10 17:34:36 +0800

    编译通过

--- server/server/udp_server/test_udp_session.cpp
+++ server/server/udp_server/test_udp_session.cpp
@@ -1,5 +1,7 @@
 #include "test_udp_session.h"
 #include "protoc_common.h"
+#include "login.pb.h"
+
 void test_udp_session::_proc_message(const message_t& msg)
 {
 
@@ -9,12 +11,15 @@ void test_udp_session::proc_message(const message_t& msg)
 
 }
 
+void test_udp_session::parseLogin(google::protobuf::Message* p, pb_flag_type flag)
+{
 
+}
 //这里负责注册消息
 void test_udp_session::registerPBCall()
 {
 
-	//registerCBFun(PROTOCO_NAME(message::MsgSaveMapReq), &Session::parseSaveMap);
+	registerCBFun(PROTOCO_NAME(message::LoginRequest), &test_udp_session::parseLogin);
 	
 
 
--- server/server/udp_server/test_udp_session.h
+++ server/server/udp_server/test_udp_session.h
@@ -1,11 +1,15 @@
 #pragma once
 #include "udpsession.h"
+#include "google/protobuf/message.h"
+#include "protoc_common.h"
 class test_udp_session :
-	public udp_session
+	public udp_session, public ProtocMsgBase<test_udp_session>
 {
+
 public:
 	virtual void _proc_message(const message_t& msg);
 	virtual void proc_message(const message_t& msg);
 	static void registerPBCall();
+	void parseLogin(google::protobuf::Message* p, pb_flag_type flag);
 };
 
