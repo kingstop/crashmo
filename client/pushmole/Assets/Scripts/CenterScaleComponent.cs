db51c487150f948a1cd663a6561d66afe4000a9c
kingstop <kingstop@sina.com>
2019-07-29 18:03:11 +0800

    add

--- client/pushmole/Assets/Scripts/CenterScaleComponent.cs
+++ client/pushmole/Assets/Scripts/CenterScaleComponent.cs
@@ -1,7 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class CenterScaleComponent : MonoBehaviour {
     private WrapContent _Content;
     private Vector2 _interval;
@@ -10,44 +10,72 @@ public class CenterScaleComponent : MonoBehaviour {
 	private bool _center = false;
 	private bool _scale_modify = false;
 	private float _scale;
+    private Image _child_image;
     void Awake()
     {
-        _pos = this.transform.localPosition;
+        //_pos = this.transform.localPosition;
+        Image[] images = GetComponentsInChildren<Image>();
+        foreach(Image image in images)
+        {
+           if(image.name == "Image_Map")
+            {
+                _child_image = image;
+                _pos = _child_image.transform.localPosition;
+                break;
+            }
+        }
+     //   _child_image = GetComponentsInChildren<Image>();
     }
     // Use this for initialization
     void Start () {
-        _pos = this.transform.localPosition;
+        //_pos = this.transform.localPosition;
     }
 	
 	// Update is called once per frame
 	void Update () {
         if(_Content != null)
         {
-			if (_center == true) {				
+			if (_center == true&& _child_image) {				
 				//this.transform.gameObject
-				Vector3 cur_pos = Camera.main.WorldToScreenPoint(this.transform.position);
-				Debug.LogWarning ("center pos [ " + cur_pos.x + "," + cur_pos.y + "," + cur_pos.z + " ]");
-				Vector2 offset = _Content.GetOffset ();
-				if (cur_pos.x >- 3400f && cur_pos.x < -1714f) {
-					float dis = offset.x * offset.x + offset.y * offset.y;
-					dis = Mathf.Sqrt (dis);
-					int offset_x = (int)(offset.x);
-					float offset_entry = offset.x - offset_x;
-					float offset_scale = Mathf.Abs (offset_entry - 0.4f);
-					offset_scale = 1.6f / (offset_scale + 1.0f);
-					_scale = offset_scale;
+                 
+				Vector3 cur_pos = this.transform.position;
+                float temp_x = UnityEngine.Screen.width / 2;
+                float temp_y = UnityEngine.Screen.height / 2;
+                float diff_x = 0f;
+                diff_x = Mathf.Abs(cur_pos.x - temp_x - 69.8f);
+                float offset_scale = 1.6f / ((diff_x + 100.0f)/100f);
+                if(offset_scale >= 1.2)
+                {
+                    offset_scale = 1.2f;
+                }
+                if(offset_scale <= 1.0f)
+                {
+                    offset_scale = 1.0f;
+                }
 
-					if (offset_scale > 1.3f) {
-						offset_scale = 1.3f;
-					}
-					float move_x = (_size.x - offset_scale * _size.x) / 2;
-					float move_y = (_size.y - offset_scale * _size.y) / 2;
-					this.transform.localScale = new Vector3 (offset_scale, offset_scale, 1);
-					this.transform.localPosition = new Vector3 (_pos.x - move_x, _pos.y - move_y, 1);
-					if (_scale_modify == false) {
-						_scale_modify = true;
-					}
-				}
+
+                //cur_p
+                //Vector2 offset = _Content.GetOffset();
+                //float dis = offset.x * offset.x + offset.y * offset.y;
+                //dis = Mathf.Sqrt(dis);
+                //int offset_x = (int)(offset.x);
+                //float offset_entry = offset.x - offset_x;
+                //float offset_scale = Mathf.Abs(offset_entry - 0.4f);
+                //offset_scale = 1.6f / (offset_scale + 1.0f);
+                //_scale = offset_scale;
+
+                //if (offset_scale > 1.3f)
+                //{
+                //    offset_scale = 1.3f;
+                //}
+                float move_x = (_size.x - offset_scale * _size.x) / 2;
+                float move_y = (_size.y - offset_scale * _size.y) / 2;
+                _child_image.transform.localScale = new Vector3(offset_scale, offset_scale, 1);
+                _child_image.transform.localPosition = new Vector3(_pos.x - move_x, _pos.y - move_y, 1);
+                if (_scale_modify == false)
+                {
+                    _scale_modify = true;
+                }
 			}
 			else 
 			{
@@ -69,10 +97,14 @@ public class CenterScaleComponent : MonoBehaviour {
 
 	public void Reset()
 	{
-		Vector3 vect_scale = new Vector3(1, 1, 1);
-		this.transform.localScale = vect_scale;
-		this.transform.localPosition = _pos;
-		_scale = 1.0f;
+		
+        if(_child_image)
+        {
+            Vector3 vect_scale = new Vector3(1, 1, 1);
+            _child_image.transform.localScale = vect_scale;
+            _child_image.transform.localPosition = _pos;
+        }
+        _scale = 1.0f;
 	}
 
 	public void setCenter(bool center)
